a952a6e05b4f2ad3c18aebbb99c1d954e616ddd1
marimedeirus <mariana_medeiros.santos@outlook.com>
2026-07-28 00:05:47 -0300

    feat: add FirstEnemy class

---
+++ FirstEnemy.java
@@ -0,0 +1,16 @@
+public class FirstEnemy {
+    
+    private long nextShot;
+
+    public void setNextShot(long nextShot) {
+        this.nextShot = nextShot;
+    }
+
+    public double getNextShot() {
+        return nextShot;
+    }
+
+    public void shoot(long currentTime){
+        
+    }
+}
