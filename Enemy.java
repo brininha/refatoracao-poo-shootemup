7af09abc88c4f6164ab8ad29bbafb75bc206073b
marimedeirus <mariana_medeiros.santos@outlook.com>
2026-07-29 00:30:26 -0300

    update: superclass Enemy

--- Enemy.java
+++ Enemy.java
@@ -1,9 +1,14 @@
-public abstract class Enemy {
+public abstract class Enemy extends Entity {
+
+    public static final int INACTIVE = 0;
+    public static final int ACTIVE = 1;
+    public static final int EXPLODING = 2;
 
     private double angle;
     private double rv;
-    private double exposionStart;
-    private double exposionEnd;
+    private double explosionStart;
+    private double explosionEnd;
+    private double v;
 
     public double getAngle() {
         return angle;
@@ -21,22 +26,78 @@ public abstract class Enemy {
         this.rv = rv;
     }
 
-    public double getExposionStart() {
-        return exposionStart;
+    public double getExplosionStart() {
+        return explosionStart;
     }
 
-    public void setExposionStart(double exposionStart) {
-        this.exposionStart = exposionStart;
+    public void setExplosionStart(double explosionStart) {
+        this.explosionStart = explosionStart;
     }
 
-    public double getExposionEnd() {
-        return exposionEnd;
+    public double getExplosionEnd() {
+        return explosionEnd;
     }
 
-    public void setExposionEnd(double exposionEnd) {
-        this.exposionEnd = exposionEnd;
+    public void setExplosionEnd(double explosionEnd) {
+        this.explosionEnd = explosionEnd;
+    }
+
+    public double getV() {
+        return v;
+    }
+
+    public void setV(double v) {
+        this.v = v;
+    }
+
+    @Override
+    public void move(long delta) {
+
+        long currentTime = System.currentTimeMillis();
+
+        if (state == EXPLODING) {
+            if (currentTime > getExplosionEnd()) {
+                state = INACTIVE;
+            }
+            return;
+        }
+
+        if (state == ACTIVE) {
+
+            x += v * Math.cos(angle) * delta;
+            y += v * Math.sin(angle) * delta * (-1.0);
+            angle += rv * delta;
+
+            if (y > GameLib.HEIGHT + radius || x < -radius || x > GameLib.WIDTH + radius) {
+                state = INACTIVE;
+                return;
+            }
+
+            shoot(currentTime);
+        }
+    }
+
+    public void explode(long currentTime) {
+        state = EXPLODING;
+        explosionStart = currentTime;
+        explosionEnd = currentTime + 500;
+    }
+
+    @Override
+    public void draw() {
+
+        if (state == EXPLODING) {
+            double alpha = (System.currentTimeMillis() - explosionStart) / (explosionEnd - explosionStart);
+            GameLib.drawExplosion(x, y, alpha);
+            return;
+        }
+
+        if (state == ACTIVE) {
+            drawShape();
+        }
     }
 
     public abstract void shoot(long currentTime);
-    
+
+    public abstract void drawShape();
 }
